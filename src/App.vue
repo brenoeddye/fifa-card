6cbf73b59bd7534a7996364a57075e78a439e1a4
Breno Pereira <brenopereira856@gmail.com>
2023-01-18 01:10:56 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+import Card from '@/components/Card.vue'
+import { defineComponent } from '@vue/runtime-core'
+
+export default defineComponent({
+  components: { Card },
+  data() {
+    return {
+
+    }
+  }
+})
+</script>
+
+<template>
+  <div class="app">
+    <Card 
+      type="wc"
+      overall="91"
+      position="ST"
+      nation="france"
+      club="PSG"
+      playerImg="some"
+      playerName="Mbappé"
+      playerPace="97"
+      playerShot="89"
+      playerPass="80"
+      playerDribling="92"
+      playerDefense="36"
+      playerPhysic="76"
+      buff="Hunter"
+    />
+  </div>
+</template>
---
+++ src/components/Card.vue
@@ -0,0 +1,64 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+        
+    },
+    props: {
+        type: String,
+        overall: String,
+        position: String,
+        nation: String,
+        club: String,
+        playerImg: String,
+        playerName: String,
+        playerPace: String,
+        playerShot: String,
+        playerPass: String,
+        playerDribling: String,
+        playerDefense: String,
+        playerPhysic: String,
+        buff: String
+    }
+})
+</script>
+
+
+<template>
+    <div :class="'card ' + type">
+        <div class="card__top">
+            <div class="card__top--ovrl">
+                <span>{{ overall }}</span>
+                <p>{{ position }}</p>
+            </div>
+            <div class="card__top--nation">
+                <img :src="'./images/nations/'+ nation + '.png'" alt="Player Nation">
+            </div>
+            <div class="card__top--club">
+                {{ club }}
+            </div>
+        </div>
+        <div class="card__image">
+            {{ playerImg }}
+        </div>
+        <div class="card__name">
+            {{ playerName }}
+        </div>
+        <div class="card__stats">
+            <ul>
+                <li>{{ playerPace }} PAC</li>
+                <li>{{ playerShot }} SHO</li>
+                <li>{{ playerPass }} PAS</li>
+            </ul>
+            <ul>
+                <li>{{ playerDribling }} DRI</li>
+                <li>{{ playerDefense }} DEF</li>
+                <li>{{ playerPhysic }} PHY</li>
+            </ul>
+        </div>
+        <div class="card__buff">
+            {{ buff }}
+        </div>
+    </div>
+</template>
