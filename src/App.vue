4fb0a0abcc22cb4457a1b474d8128af0c251457d
Breno Pereira <brenopereira856@gmail.com>
2023-01-22 12:54:50 -0300

    update card

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script lang="ts">
 import Card from '@/components/Card.vue'
+import Steps from '@/components/Steps.vue'
 import { defineComponent } from '@vue/runtime-core'
 
 export default defineComponent({
-  components: { Card },
+  components: { Card, Steps },
   data() {
     return {
-
+      playerOver: '99',
+      playerName: 'Seu nome'
     }
   }
 })
@@ -14,15 +16,29 @@ export default defineComponent({
 
 <template>
   <div class="app">
+    <div class="menu">
+        <h1>Criação de Card</h1>
+        <div class="menu__content">
+          <section class="menu__content--section">
+            <h2>Overal e Posição</h2>
+            <input type="number" v-model="playerOver" max="99" min="0" maxlength="2"/>
+            <input type="text" v-model="playerName" maxlength="20"/>
+          </section>
+          <section class="menu__content--section">
+            <h2>País e clube</h2>
+          </section>
+        </div>
+    </div>
+
     <Card 
-      type="wc"
-      overall="91"
+      type="gold"
+      :overall="playerOver"
       position="ST"
       nation="france"
       league="ligue1"
       club="psg"
       playerImg="guest"
-      playerName="Mbappé"
+      :playerName="playerName"
       playerPace="97"
       playerShot="89"
       playerPass="80"
@@ -31,5 +47,7 @@ export default defineComponent({
       playerPhysic="76"
       buff="hunter"
     />
+
+    <Steps />
   </div>
 </template>
---
+++ src/components/Steps.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    
+})
+</script>
+
+<template>
+    <div class="steps">
+        <div id="step-1" class="steps__step">
+
+        </div>
+        <div id="step-2" class="steps__step">
+
+        </div>
+        <div id="step-3" class="steps__step">
+
+        </div>
+        <div id="step-4" class="steps__step">
+
+        </div>
+    </div>
+</template>
